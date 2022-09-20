f2e08b55b56168656faed0efcddf8b1bee312536
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2022-09-20 16:18:06 +0200

    Labels Update

--- M7/M7_1a10/src/VisorDeImagen.java
+++ M7/M7_1a10/src/VisorDeImagen.java
@@ -1,9 +1,6 @@
 import java.awt.Container;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -12,77 +9,75 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class VisorDeImagen {
-    private JFrame ventana;
 
+    public static void main(String[] args) {
+        VisorDeImagen visorDeImagen = new VisorDeImagen();
+    }
     public VisorDeImagen() {
         construirVentana();
     }
 
     private void construirVentana() {
-        ventana = new JFrame("Visor de Imagenes");
+        JFrame ventana = new JFrame("Visor de Imagenes");
 
         Container panelContenedor = ventana.getContentPane();
-        panelContenedor  =  ventana.getContentPane();
-
-        JLabel etiquetaNombreDeArchivo = new JLabel("wrngwalenfweinfowienfwoienfowienfowienf");
-        panelContenedor.add(etiquetaNombreDeArchivo);
+        panelContenedor  =  ventana. getContentPane ();
 
         Container panelDeImagen = new JPanel();
         panelContenedor.add(panelDeImagen);
 
-        JLabel etiquetaEstado  =  new  JLabel("Version  1 .O");
+        JLabel etiquetaNombreDeArchivo = new JLabel("JLabel - 1");
+        panelContenedor.add(etiquetaNombreDeArchivo);
+
+        JLabel etiquetaEstado  =  new  JLabel("JLabel - 2");
         panelContenedor.add(etiquetaEstado);
 
-        construirBarraDeMenu(ventana);
+        BarraDeMenu(ventana);
         ventana.pack();
         ventana.setSize(400, 400);
         ventana.setVisible(true);
     }
-    public void construirBarraDeMenu(JFrame ventana) {
+    public void BarraDeMenu(JFrame ventana) {
         JMenuBar barraDeMenu = new JMenuBar();
         ventana.setJMenuBar(barraDeMenu);
 
         JMenu menuArchivo = new JMenu("Archivo");
         barraDeMenu.add(menuArchivo);
-
         JMenuItem elementoAbrir = new JMenuItem("Abrir");
+        menuArchivo.add(elementoAbrir);
+        JMenuItem elementoGrabar = new JMenuItem("Grabar");
+        menuArchivo.add(elementoGrabar);
+        JMenuItem elementoSalir = new JMenuItem("Salir");
+        menuArchivo.add(elementoSalir);
+        JMenu menuAyuda = new JMenu("Ayuda");
+        barraDeMenu.add(menuAyuda);
+        JMenuItem elementoAcercaDe = new JMenuItem("Acerca del Visor de Imagenes");
+
         elementoAbrir.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { abrirArchivo(); }
         });
-        menuArchivo.add(elementoAbrir);
-
-        JMenuItem elementoGrabar = new JMenuItem("Grabar");
         elementoGrabar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { grabarArchivo(); }
         });
-        menuArchivo.add(elementoGrabar);
-
-        JMenuItem elementoSalir = new JMenuItem("Salir");
         elementoSalir.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { salir(); }
         });
-
-        menuArchivo.add(elementoSalir);
-
-        JMenu menuAyuda = new JMenu("Ayuda");
-        barraDeMenu.add(menuAyuda);
-
-        JMenuItem elementoAcercaDe = new JMenuItem("Acerca del Visor de Imagenes");
         elementoAcercaDe.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { acercaDe(); }
         });
         menuAyuda.add(elementoAcercaDe);
     }
     private void abrirArchivo() {
-        System.out.println("Has seleccionado el item: Abrir.");
+        System.out.println("Abrir.");
     }
     private void grabarArchivo() {
-        System.out.println("Has seleccionado el item: Grabar.");
+        System.out.println("Grabar");
     }
     private void salir() {
-        System.out.println("Has seleccionado el item: Salir.");
+        System.out.println("Salir");
+        System.exit(0);
     }
     private void acercaDe() {
-        System.out.println("Has seleccionado el item: Acerca del Visor de ImÃ¡genes.");
+        System.out.println("Ayuda");
     }
 }
