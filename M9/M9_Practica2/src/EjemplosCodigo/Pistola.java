fbaac304bdf2147807ea7f5766536bec8e6ac45f
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2022-09-27 18:23:42 +0200

    M9_SynchronizeThreads Release v2

--- M9/M9_Practica2/src/EjemplosCodigo/Pistola.java
+++ M9/M9_Practica2/src/EjemplosCodigo/Pistola.java
@@ -2,14 +2,14 @@ package EjemplosCodigo;
 
 public class Pistola {
     private boolean enposicio = true;
-    public synchronized void disparar(int cartucho) {
+    public void disparar(int cartucho) {
         while (enposicio == false) {
             try { wait(); } catch (InterruptedException e) { }
         }
         enposicio = false;
         notifyAll();
     }
-    public synchronized void apuntar() {
+    public void apuntar() {
         while (enposicio == true) {
             try { wait(); } catch (InterruptedException e) { }
         }
